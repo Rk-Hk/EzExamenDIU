5280f24dd894dd44ed15fcffcf6716e34f408108
Rk-Hk <ricardobdark@gmail.com>
2017-11-10 06:09:26 -0500

    vista principal , selector de cuenta y menu principal

---
+++ assets/js/dynamic.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".examen-option").on("click", function(){
+        window.location="../index.html";
+    });
+    $(".content-main button").on("click",function(){
+        
+    })
+    
+});
