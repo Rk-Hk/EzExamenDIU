2c1e4e60739c410ef877e73e882724c3a32a141b
Rk-Hk <ricardobdark@gmail.com>
2017-11-10 22:02:02 -0500

    Se agrego login , registro, pagina de carga, videos

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
+     
     $(".examen-option").on("click", function(){
         window.location="../index.html";
     });
+    $(".video-option").on("click", function(){
+        window.location="videotutoriales.html";
+    });
     $(".content-main button").on("click",function(){
         
-    })
+    });
+    $(".btn-volver").on("click", function(){
+        window.location="home.html";
+    });
+    $(".btn-empezar").on("click", function(){
+        window.location="views/login.html";
+    });
+    $(".btn-login").on("click", function(){
+        alert("asdas");
+        window.location="/home.html";
+    });
+    
+    
     
 });
