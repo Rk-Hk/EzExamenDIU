5f8dd2d669c7529e0a5112d8d887da0b09fb699c
Rk-Hk <ricardobdark@gmail.com>
2017-11-11 13:17:01 -0500

    Avanza mrd , no pusheas nada :V

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-     
+     var estadoSidebar = false;
     $(".examen-option").on("click", function(){
         window.location="../index.html";
     });
@@ -15,11 +15,18 @@ $(document).ready(function(){
     $(".btn-empezar").on("click", function(){
         window.location="views/login.html";
     });
-    $(".btn-login").on("click", function(){
-        alert("asdas");
-        window.location="/home.html";
+    $("#btn-login").on("click", function(){
+        window.location="home.html";
     });
-    
-    
+    $(".btn-sidebar").on("click", function(){
+        estadoSidebar=!estadoSidebar;
+        if(estadoSidebar){
+            
+            $("#sidebar").fadeOut();
+        }else{
+            $("#sidebar").fadeIn();
+        }
+        
+    })
     
 });
