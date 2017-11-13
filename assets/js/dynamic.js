a9509dd76c7df3755e5dd7cd2090462ea6886e66
Israel Ruiz <israel_r93@hotmail.com>
2017-11-13 16:22:32 -0500

    Añadiendo tarea Examenes, revisa mis estilos css si tienes algun problema perra

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,32 +1,40 @@
-$(document).ready(function(){
-     var estadoSidebar = false;
-    $(".examen-option").on("click", function(){
-        window.location="../index.html";
-    });
-    $(".video-option").on("click", function(){
-        window.location="videotutoriales.html";
-    });
-    $(".content-main button").on("click",function(){
-        
-    });
-    $(".btn-volver").on("click", function(){
-        window.location="home.html";
-    });
-    $(".btn-empezar").on("click", function(){
-        window.location="views/login.html";
-    });
-    $("#btn-login").on("click", function(){
-        window.location="home.html";
-    });
-    $(".btn-sidebar").on("click", function(){
-        estadoSidebar=!estadoSidebar;
-        if(estadoSidebar){
-            
-            $("#sidebar").fadeOut();
-        }else{
-            $("#sidebar").fadeIn();
-        }
-        
-    })
-    
+$(document).ready(function() {
+  var estadoSidebar = false;
+  $(".examen-option").on("click", function() {
+    window.location = "modo.html";
+  });
+  $(".video-option").on("click", function() {
+    window.location = "videotutoriales.html";
+  });
+  $(".content-main button").on("click", function() {});
+  $(".btn-volver").on("click", function() {
+    window.location = "home.html";
+  });
+  $(".btn-empezar").on("click", function() {
+    window.location = "views/login.html";
+  });
+  $("#btn-login").on("click", function() {
+    window.location = "home.html";
+  });
+  $(".item-university").on("click", function() {
+    window.location = "listaExamenes.html";
+  });
+  $(".modo-option").on("click", function() {
+    window.location = "universidades.html";
+  });
+  $(".btn-sidebar").on("click", function() {
+    estadoSidebar = !estadoSidebar;
+    if (estadoSidebar) {
+      $("#sidebar").fadeOut();
+    } else {
+      $("#sidebar").fadeIn();
+    }
+  });
+  $(".container-examenes").on("click", function() {
+    $("#sidebar").fadeOut();
+  });
+  $("#empezarExamen").on("click", function() {
+    window.location = "mapaUniversidad.html";
+  });
+  
 });
